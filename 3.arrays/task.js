e179c405e4aa57da3e8dfeb11cc51aeaaf0cd1ca
Slava netologia <s-29@mail.ru>
2024-10-08 09:33:50 +0300

    Task 3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,18 @@
 function compareArrays(arr1, arr2) {
-  
+
+  if (arr1.length!=arr2.length){
+    return false;
+  }
+  return arr1.every((value,index)=>value===arr2[index]);
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+
+    let usersFilter = users.filter(user=>user.gender===gender);
+    if (usersFilter.length==0){
+        return 0;
+    }
+    let result = usersFilter.map(user=>user.age).reduce((sum,age)=>sum+age,0)/usersFilter.length;
+
+    return result;
 }
